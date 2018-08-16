f2abb01dea37ac6a591ec485c09e888ba97d10af
Michael Uvarov <arcusfelis@gmail.com>
2018-08-16 14:42:28 +0200

    Add monitoring_demo_app:run_demo/0

--- README.md
+++ README.md
@@ -7,3 +7,42 @@ Build
 -----
 
     $ rebar3 compile
+
+
+Example
+-------
+
+```erlang
+./rebar3 shell
+
+application:ensure_all_started(monitoring_demo).
+ok
+
+3> monitoring_demo_app:run_demo().
+14:41:16.204 [info] event=run_demo sleep_time=444 milliseconds time=444654
+microseconds
+ok
+4> monitoring_demo_app:run_demo().
+ok
+14:41:18.449 [info] event=run_demo sleep_time=723 milliseconds time=724187
+microseconds
+5> monitoring_demo_app:run_demo().
+ok
+14:41:20.686 [info] event=run_demo sleep_time=946 milliseconds time=947505
+microseconds
+
+7> exometer:get_values([]).
+[{[demo,runs],[{count,3},{one,3}]},
+ {[demo,time],
+  [{n,5},
+   {mean,705448},
+   {min,444654},
+   {max,947505},
+   {median,724187},
+   {50,724187},
+   {75,947505},
+   {90,947505},
+   {95,947505},
+   {99,947505},
+   {999,947505}]}]
+```
--- src/monitoring_demo_app.erl
+++ src/monitoring_demo_app.erl
@@ -10,15 +10,34 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+-export([run_demo/0]).
+
+
+% Enable lager
+-compile([{parse_transform, lager_transform}]).
+
 %%====================================================================
 %% API
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    lager:info("event=starting_monitoring_demo_app", []),
+    exometer:new([demo, runs], spiral),
+    exometer:new([demo, time], histogram),
     monitoring_demo_sup:start_link().
 
 %%--------------------------------------------------------------------
 stop(_State) ->
+    ok.
+
+
+run_demo() ->
+    SleepTime = round(random:uniform() * 1000), % 0 - 1000 milliseconds
+    {Time, _} = timer:tc(fun() -> timer:sleep(SleepTime) end),
+    lager:info("event=run_demo sleep_time=~p milliseconds time=~p microseconds",
+               [SleepTime, Time]),
+    exometer:update([demo, time], Time),
+    exometer:update([demo, runs], 1),
     ok.
 
 %%====================================================================
