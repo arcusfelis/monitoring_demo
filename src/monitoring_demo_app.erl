026f8b6d82ef7f7c208b0782227bc00b33d5d444
Michael Uvarov <arcusfelis@gmail.com>
2018-08-16 14:49:13 +0200

    Add comments

--- src/monitoring_demo_app.erl
+++ src/monitoring_demo_app.erl
@@ -22,7 +22,10 @@
 
 start(_StartType, _StartArgs) ->
     lager:info("event=starting_monitoring_demo_app", []),
-    exometer:new([demo, runs], spiral),
+    % spiral counter counts:
+    % - total nuber of calls
+    % - number of calls in one minute sliding window
+    exometer:new([demo, runs], spiral), % creates a spiral counter
     exometer:new([demo, time], histogram),
     monitoring_demo_sup:start_link().
 
@@ -31,12 +34,19 @@ stop(_State) ->
     ok.
 
 
+%% This function sleeps random time and reports it
+%% In a real world example, replace timer:sleep/1 with:
+%% - database query function
+%% - a request handler function
+%% ...
+%%
+%% [demo, runs] and [demo, time] are metric names.
 run_demo() ->
     SleepTime = round(random:uniform() * 1000), % 0 - 1000 milliseconds
     {Time, _} = timer:tc(fun() -> timer:sleep(SleepTime) end),
     lager:info("event=run_demo sleep_time=~p milliseconds time=~p microseconds",
                [SleepTime, Time]),
-    exometer:update([demo, time], Time),
+    exometer:update([demo, time], Time), % updates histogram
     exometer:update([demo, runs], 1),
     ok.
 
